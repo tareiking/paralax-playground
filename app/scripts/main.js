f971bb66f72abd1b3bdf25cf99ae4aa54ccc736d
Tarei King <tareiking@gmail.com>
2014-11-03 20:26:01 +1000

    Add html skeleton

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+// init controller
+var controller = new ScrollMagic();
+
+// assign handler "scene" and add it to controller
+var scene = new ScrollScene({duration: 300})
+                .setPin("img.left-image") // pins the element for a scroll distance of 100px
+                .addTo(controller); // add scene to controller
